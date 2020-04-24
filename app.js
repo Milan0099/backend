d3e8a3441181a3f08175a1ac389366f3aa6674db
timon0305 <timon0305@outlook.com>
2020-04-24 23:12:34 +0200

    first step backend completed

--- app.js
+++ app.js
@@ -8,8 +8,7 @@ const userRouter = require('./api/users/user.router');
 app.use(express.json());
 app.use(cors());
 
-app.use('/api/users', userRouter
-    );
+app.use('/api/users', userRouter);
 
 app.listen(process.env.APP_PORT, () => {
     console.log('Server up and running', process.env.APP_PORT);
